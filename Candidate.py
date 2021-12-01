a7b937d65d826d655b69c26ce58d5138e5316128
Nirisha B <68464594+Nirishab7@users.noreply.github.com>
2021-12-01 09:32:42 +0530

    Update Candidate.py

--- Candidate.py
+++ Candidate.py
@@ -1,48 +1,114 @@
-import numpy as np 
-import pandas as pd
 
-data = pd.read_csv('candidate.csv')
-#print(data)
-concepts = np.array(data.iloc[:,:-1])
+import csv
+def g_0(n):
+    return ("?",)*n
 
-print("\nInstances are:\n",concepts)
-target = np.array(data.iloc[:,-1])
-print("\nTarget Values are: ",target)
+def s_0(n):
+    return ('0',)*n 
 
-def learn(concepts, target): 
-    specific_h = concepts[0].copy()
-    print("\nInitialization of specific_h and genearal_h")
-    print("\nSpecific Boundary: ", specific_h)
-    general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]
-    print("\nGeneric Boundary: ",general_h)  
+def more_general(h1, h2):
+    more_general_parts = []
+    for x, y in zip(h1, h2):
+        mg = x == "?" or (x != "0" and (x == y or y == "0"))
+        more_general_parts.append(mg)
+    return all(more_general_parts)
 
-    for i, h in enumerate(concepts):
-        print("\nInstance", i+1 , "is ", h)
-        if target[i] == "yes":
-            print("Instance is Positive ")
-            for x in range(len(specific_h)): 
-                if h[x]!= specific_h[x]:                    
-                    specific_h[x] ='?'                     
-                    general_h[x][x] ='?'
-                   
-        if target[i] == "no":            
-            print("Instance is Negative ")
-            for x in range(len(specific_h)): 
-                if h[x]!= specific_h[x]:                    
-                    general_h[x][x] = specific_h[x]                
-                else:                    
-                    general_h[x][x] = '?'        
+def fulfills(example, hypothesis):
+    ### the implementation is the same as for hypotheses:
+    return more_general(hypothesis, example)
+
+def min_generalizations(h, x):
+    h_new = list(h)
+    for i in range(len(h)):
+        if not fulfills(x[i:i+1], h[i:i+1]):
+            h_new[i] = '?' if h[i] != '0' else x[i]
+    return [tuple(h_new)]
+
+min_generalizations(h=('0', '0'  , 'sunny'),x=('rainy', 'windy', 'cloudy'))
+
+def min_specializations(h, domains, x):
+    results = []
+    for i in range(len(h)):
+        if h[i] == "?":
+            for val in domains[i]:
+                if x[i] != val:
+                    h_new = h[:i] + (val,) + h[i+1:]
+                    results.append(h_new)
+        elif h[i] != "0":
+            h_new = h[:i] + ('0',) + h[i+1:]
+            results.append(h_new)
+    return results
+
+min_specializations(h=('?', 'x',), 
+                    domains=[['a', 'b', 'c'], ['x', 'y']], 
+                    x=('b', 'x'))
+
+with open('data21_shape.csv')  as csvFile:
+        examples = [tuple(line) for line in csv.reader(csvFile)]
         
-        print("Specific Bundary after ", i+1, "Instance is ", specific_h)         
-        print("Generic Boundary after ", i+1, "Instance is ", general_h)
-        print("\n")
+print(examples)
 
-    indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]    
-    for i in indices:   
-        general_h.remove(['?', '?', '?', '?', '?', '?']) 
-    return specific_h, general_h 
+def get_domains(examples):
+    d = [set() for i in examples[0]]
+    for x in examples:
+        for i, xi in enumerate(x):
+            d[i].add(xi)
+    return [list(sorted(x)) for x in d]
 
-s_final, g_final = learn(concepts, target)
+get_domains(examples)
 
-print("Final Specific_h: ", s_final, sep="\n")
-print("Final General_h: ", g_final, sep="\n")
+def candidate_elimination(examples):
+    domains = get_domains(examples)[:-1]
+    
+    G = set([g_0(len(domains))])
+    S = set([s_0(len(domains))])
+    i=0
+    print("\n G[{0}]:".format(i),G)
+    print("\n S[{0}]:".format(i),S)
+    for xcx in examples:
+        i=i+1
+        x, cx = xcx[:-1], xcx[-1]  # Splitting data into attributes and decisions
+        if cx=='Y': # x is positive example
+            G = {g for g in G if fulfills(x, g)}
+            S = generalize_S(x, G, S)
+        else: # x is negative example
+            S = {s for s in S if not fulfills(x, s)}
+            G = specialize_G(x, domains, G, S)
+        print("\n G[{0}]:".format(i),G)
+        print("\n S[{0}]:".format(i),S)
+    return 
+def generalize_S(x, G, S):
+    S_prev = list(S)
+    for s in S_prev:
+        if s not in S:
+            continue
+        if not fulfills(x, s):
+            S.remove(s)
+            Splus = min_generalizations(s, x)
+            ## keep only generalizations that have a counterpart in G
+            S.update([h for h in Splus if any([more_general(g,h) 
+                                               for g in G])])
+            ## remove hypotheses less specific than any other in S
+            S.difference_update([h for h in S if 
+                                 any([more_general(h, h1) 
+                                      for h1 in S if h != h1])])
+    return S
+
+def specialize_G(x, domains, G, S):
+    G_prev = list(G)
+    for g in G_prev:
+        if g not in G:
+            continue
+        if fulfills(x, g):
+            G.remove(g)
+            Gminus = min_specializations(g, domains, x)
+            ## keep only specializations that have a conuterpart in S
+            G.update([h for h in Gminus if any([more_general(h, s)
+                                                for s in S])])
+            ## remove hypotheses less general than any other in G
+            G.difference_update([h for h in G if 
+                                 any([more_general(g1, h) 
+                                      for g1 in G if h != g1])])
+    return G
+
+print(candidate_elimination(examples))
